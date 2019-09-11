75bc44f8525aa5a3bea80c40107848b8af599773
Jihong Kim <jk279242naver.com>
2019-09-11 18:28:00 -0500

    kept old models, and created another model and config.json, so I have new models directory with burger.js. And matched the server to my server configuration.

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,9 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
-  database: "burgers_db"
+  password: "root",
+  database: "burgers_db",
+  port: 8889
 });
 
 connection.connect(function(err) {
